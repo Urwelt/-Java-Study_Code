99138ab8bd4c35289cd9cab7757f66a0ec5d6168
geytoea12 <giturweltcode12@gmail.com>
2020-05-16 12:05:44 +0900

    String型をBufferdReaderで文字列結合

---
+++ StringToBufferdReader/src/StringToBufferdReader.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class StringToBufferdReader {
+    public static void main(String[] args) {
+    	final String addString = "Best in";
+    	
+    	try(Scanner in = new Scanner(System.in)){
+    		StringBuffer buf = new StringBuffer();
+    		String inputStr = in.nextLine();
+    		buf.append(addString);
+    		buf.append(" ");
+    		buf.append(inputStr); 
+    		System.out.println(buf);
+    	}catch(Exception e) {
+    		e.printStackTrace();
+    	}    	    
+   	}
+}
